958718a69dbd0263a788a60646df47717d1e4348
rahgirrafi <rahgirrafi@gmail.com>
2025-07-07 15:32:01 +0600

    First comment. Package built succesfully. Run test need to be done

--- build/arduino_blink/CMakeFiles/arduino_blink_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/arduino_blink_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/arduino_blink_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/serial/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /run/media/rafi/Technical/Github/ws_ros2_control_blink/src/serial
-# Build directory: /run/media/rafi/Technical/Github/ws_ros2_control_blink/build/serial
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
